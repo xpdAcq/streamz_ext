2ec3ebc48c96bc36b475f2976213cfc0f81fd156
christopher <cjwright4242@gmail.com>
2018-01-11 13:53:18 -0500

    TST: test execution order

--- test/test_core.py
+++ test/test_core.py
@@ -24,3 +24,32 @@ def test_unique_dict():
     source.emit({'a': 1})
 
     assert L == [{'a': 1}]
+
+
+def test_execution_order():
+    L = []
+    for i in range(5):
+        s = Stream()
+        b = s.pluck(1)
+        a = s.pluck(0)
+        l = a.combine_latest(b, emit_on=a).sink_to_list()
+        z = [(1, 'red'), (2, 'blue'), (3, 'green')]
+        for zz in z:
+            s.emit(zz)
+        L.append((l, ))
+    for ll in L:
+        assert ll == L[0]
+
+    L2 = []
+    for i in range(5):
+        s = Stream()
+        a = s.pluck(0)
+        b = s.pluck(1)
+        l = a.combine_latest(b, emit_on=a).sink_to_list()
+        z = [(1, 'red'), (2, 'blue'), (3, 'green')]
+        for zz in z:
+            s.emit(zz)
+        L2.append((l,))
+    for ll, ll2 in zip(L, L2):
+        assert ll2 == L2[0]
+        assert ll != ll2
