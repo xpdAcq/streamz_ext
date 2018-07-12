8d2a1a3a2ee85d0c143e78e93036a6840f624b40
christopher <cjwright4242@gmail.com>
2018-07-12 00:38:38 -0400

    fix the add_edge logic to properly connect and disconnect upstreams according to their name representation.

--- streamz_ext/link.py
+++ streamz_ext/link.py
@@ -1,41 +1,22 @@
+import networkx as nx
+
+from .graph import StreamzGraph
 
 
-# TODO: should the inputs and outputs be defined as kwargs for the pipeline
-# generating function?
-# This would make the requirements more explicit and have a linker error
-# maybe we use sig f(*, kwarg1, kwarg2, **kwargs)?
-def micro_link(input_graph, output_graph):
-    """Link to dicts of sub graphs together by their keys
-
-    Parameters
-    ----------
-    input_graph : dict of Streams
-    output_graph : dict of Streams
-    """
-    for name in output_graph:
-        if name in input_graph:
-            input_graph[name].connect(output_graph[name])
-    # TODO: idiomatic way to do this? (essentially left update)
-    input_graph.update(
-        {k: v for k, v in output_graph.items() if k not in input_graph})
-
-
-# TODO: support bypass? Maybe that should be a separate pipeline optimization
-# step
 def link(*args):
     """Link a series of sub graphs in order
 
     Parameters
     ----------
-    args : iterable of dicts of Streams
+    args : iterable of StreamzGraph
 
     Returns
     -------
-    dict of Streams:
-        The the final pipeline dict
+    StreamzGraph:
+        The the final pipeline StreamzGraph
 
     """
-    total_graph = {}
+    total_graph = StreamzGraph()
     for graph in args:
-        micro_link(total_graph, graph)
+        total_graph = nx.compose(graph, total_graph)
     return total_graph
--- test/test_link.py
+++ test/test_link.py
@@ -1,5 +1,5 @@
 from streamz_ext.link import *
-from streamz_ext import Stream
+from streamz_ext import Stream, create_streamz_graph
 try:
     from streamz.tests.test_link import *
 except ImportError:
@@ -8,105 +8,54 @@ except ImportError:
 
 def test_link():
     def make_a():
-        source = Stream()
-        out_a = source.map(lambda x: x + 1)
-        return {'in_a': source, 'out_a': out_a}
+        out_a = Stream(stream_name='in_a').map(lambda x: x + 1,
+                                               stream_name='out_a')
+        return create_streamz_graph(out_a)
 
     def make_b():
-        out_a = Stream()
-        out_b = out_a.map(lambda x: x * 2)
-        return {'out_a': out_a, 'out_b': out_b}
+        out_b = Stream(stream_name='out_a').map(lambda x: x * 2,
+                                                stream_name='out_b')
+        return create_streamz_graph(out_b)
     a = make_a()
     b = make_b()
-    L = b['out_b'].sink_to_list()
+    L = b.node['out_b']['node'].sink_to_list()
     for i in range(10):
-        a['in_a'].emit(i)
+        a.node['in_a']['node'].emit(i)
     assert len(L) == 0
-    link(a, b)
+    z = link(a, b)
     for i in range(10):
-        a['in_a'].emit(i)
+        a.node['in_a']['node'].emit(i)
     assert L == [(i + 1) * 2 for i in range(10)]
-
-
-# def test_link_bypass():
-#     def make_a():
-#         source = Stream()
-#         out_a = source.map(lambda x: x + 1)
-#         return {'in_a': source, 'out_a': out_a}
-#
-#     def make_b():
-#         out_a = Stream()
-#         out_b = out_a.map(lambda x: x * 2)
-#         return {'out_a': out_a, 'out_b': out_b}
-#     a = make_a()
-#     b = make_b()
-#     L = b['out_b'].sink_to_list()
-#     for i in range(10):
-#         a['in_a'].emit(i)
-#     assert len(L) == 0
-#     link(a, b, bypass=True)
-#     for i in range(10):
-#         a['in_a'].emit(i)
-#     assert L == [(i + 1) * 2 for i in range(10)]
 
 
 def test_double_link():
     def make_a():
-        source = Stream()
-        out_a = source.map(lambda x: x + 1)
-        return {'in_a': source, 'out_a': out_a}
+        out_a = Stream(stream_name='in_a').map(lambda x: x + 1,
+                                               stream_name='out_a')
+        return create_streamz_graph(out_a)
 
     def make_b():
-        out_a = Stream()
-        out_b = out_a.map(lambda x: x * 2)
-        return {'out_a': out_a, 'out_b': out_b}
+        out_b = Stream(stream_name='out_a').map(lambda x: x * 2,
+                                                stream_name='out_b')
+        return create_streamz_graph(out_b)
 
     def make_c():
-        out_a = Stream()
-        out_b = Stream()
-        out_c = out_a.zip(out_b).map(sum)
-        return {'out_a': out_a, 'out_b': out_b, 'out_c': out_c}
+        a = Stream(stream_name='out_a')
+        b = Stream(stream_name='out_b')
+        a_zip = a.zip(b, stream_name='z')
+        out_c = a_zip.map(sum, stream_name='out_c')
+        return create_streamz_graph(out_c)
 
     a = make_a()
     b = make_b()
-    L = b['out_b'].sink_to_list()
+    L = b.node['out_b']['node'].sink_to_list()
     ab = link(a, b)
     c = make_c()
-    L2 = c['out_c'].sink_to_list()
+    L2 = c.node['out_c']['node'].sink_to_list()
     abc = link(ab, c)
+    assert len(c.node['z']['node'].upstreams) == 2
 
     for i in range(10):
-        a['in_a'].emit(i)
+        abc.node['in_a']['node'].emit(i)
     assert L == [(i + 1) * 2 for i in range(10)]
     assert L2 == [((i + 1) * 2) + i + 1 for i in range(10)]
-
-
-# def test_double_link_bypass():
-#     def make_a():
-#         source = Stream()
-#         out_a = source.map(lambda x: x + 1)
-#         return {'in_a': source, 'out_a': out_a}
-#
-#     def make_b():
-#         out_a = Stream()
-#         out_b = out_a.map(lambda x: x * 2)
-#         return {'out_a': out_a, 'out_b': out_b}
-#
-#     def make_c():
-#         out_a = Stream()
-#         out_b = Stream()
-#         out_c = out_a.zip(out_b).map(sum)
-#         return {'out_a': out_a, 'out_b': out_b, 'out_c': out_c}
-#
-#     a = make_a()
-#     b = make_b()
-#     L = b['out_b'].sink_to_list()
-#     ab = link(a, b, bypass=True)
-#     c = make_c()
-#     L2 = c['out_c'].sink_to_list()
-#     abc = link(ab, c, bypass=True)
-#
-#     for i in range(10):
-#         a['in_a'].emit(i)
-#     assert L == [(i + 1) * 2 for i in range(10)]
-#     assert L2 == [((i + 1) * 2) + i + 1 for i in range(10)]
