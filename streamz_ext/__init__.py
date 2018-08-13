aec1a86ebe385b412572074d70f7497d58fc0ab2
christopher <cjwright4242@gmail.com>
2018-08-13 07:40:34 -0400

    wip

--- streamz_ext/__init__.py
+++ streamz_ext/__init__.py
@@ -1,3 +1,10 @@
 __version__ = '0.2.1'
 
 from .core import *
+from .graph import *
+from .sources import *
+from .thread import *
+try:
+    from .dask import DaskStream, scatter
+except ImportError:
+    pass
