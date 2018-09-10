a356bd37173e8ffba15baf496498d50d67bab4cc
christopher <cjwright4242@gmail.com>
2018-09-09 20:50:17 -0400

    merge with streamz

--- streamz_ext/__init__.py
+++ streamz_ext/__init__.py
@@ -1,3 +1,9 @@
-__version__ = '0.2.1'
-
 from .core import *
+from .graph import *
+from .sources import *
+try:
+    from .dask import DaskStream, scatter
+except ImportError:
+    pass
+
+__version__ = '0.2.2'
---
+++ streamz_ext/tests/test_graph.py
@@ -0,0 +1,90 @@
+from operator import add, mul
+import os
+
+import pytest
+nx = pytest.importorskip('networkx')
+
+from .. import Stream, create_graph, visualize
+from ..utils_test import tmpfile
+
+from ..graph import _clean_text, run_vis, node_style
+
+
+def test_create_graph():
+    source1 = Stream(stream_name='source1')
+    source2 = Stream(stream_name='source2')
+
+    n1 = source1.zip(source2)
+    n2 = n1.map(add)
+    n2.sink(source1.emit)
+
+    g = nx.DiGraph()
+    create_graph(n2, g)
+    for t in [hash(a) for a in [source1, source2, n1, n2]]:
+        assert t in g
+
+
+def test_create_file():
+    source1 = Stream(stream_name='source1')
+    source2 = Stream(stream_name='source2')
+
+    n1 = source1.zip(source2)
+    n2 = n1.map(add).scan(mul).map(lambda x : x + 1)
+    n2.sink(source1.emit)
+
+    with tmpfile(extension='png') as fn:
+        visualize(n1, filename=fn)
+        assert os.path.exists(fn)
+
+    with tmpfile(extension='svg') as fn:
+        n1.visualize(filename=fn, rankdir="LR")
+        assert os.path.exists(fn)
+
+    with tmpfile(extension='dot') as fn:
+        n1.visualize(filename=fn, rankdir="LR")
+        with open(fn) as f:
+            text = f.read()
+
+        for word in ['rankdir', 'source1', 'source2', 'zip', 'map', 'add',
+                     'shape=box', 'shape=ellipse']:
+            assert word in text
+
+
+def test_cleantext():
+    text = "JFDSM*(@&$:FFDS:;;"
+    expected_text = "JFDSM ;FFDS; "
+    cleaned_text = _clean_text(text)
+    assert cleaned_text == expected_text
+
+
+def test_run_vis_smoke():
+    import matplotlib.pyplot as plt
+    source = Stream()
+
+    def sleep_inc(x):
+        if x == 5:
+            raise RuntimeError()
+        return x + 1
+
+    def print_sleep(x):
+        print(x)
+
+    b = source.map(sleep_inc)
+    b.sink(print_sleep)
+    b.sink(print_sleep)
+    gv = run_vis(
+        source,
+        source_node=True,
+        edge_style={"color": "k"},
+        node_label_style={"font_size": 10},
+        edge_label_style=lambda x: {"label": x["label"], "font_size": 15},
+        node_style=node_style,
+        force_draw=True,
+    )
+    plt.pause(.1)
+    for i in range(10):
+        try:
+            source.emit(i)
+            plt.pause(.1)
+        except RuntimeError:
+            pass
